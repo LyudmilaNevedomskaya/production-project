d8ede29f04fffbf8265fe7f8ee1a27930e0ecad8
Lyudmila Nevedomskaya <lyudmila@admachtec.com>
2024-04-24 13:07:01 -0700

    App Router - route Config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Suspense } from "react";
 import { Link } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { classNames } from "../shared/lib/classNames/classNames";
 import { useTheme } from "./providers";
-import AboutPage from "src/pages/AboutPage/ui/AboutPage";
-import MainPage from "src/pages/MainPage/ui/MainPage";
+import { AppRouter } from "./providers/router";
 
 
 
@@ -16,12 +13,7 @@ const App = () => {
       <button onClick={toggleTheme}>Theme</button>
       <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
