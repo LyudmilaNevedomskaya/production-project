79d226651a302761df9bd58cdc8dc3db0c082ec6
Lyudmila Nevedomskaya <lyudmila@admachtec.com>
2024-04-24 12:42:13 -0700

    Absolute imports

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
-import {ResolveOptions} from "webpack";
+import { ResolveOptions } from "webpack";
+import path from "path";
 
 export function buildResolvers(): ResolveOptions {
-    return {
-        extensions: ['.tsx', '.ts', '.js'],
-    }
+  return {
+    extensions: [".tsx", ".ts", ".js"],
+    preferAbsolute: true,
+    alias: {
+      src: path.resolve("./src"),
+    },
+  };
 }
