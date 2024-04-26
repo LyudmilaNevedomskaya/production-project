9b396e9a716fbc8976b547e7d5e5dd42dbbb62c3
Lyudmila Nevedomskaya <lyudmila@admachtec.com>
2024-04-26 13:37:54 -0700

    Button shared component

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import { classNames } from "src/shared/lib/classNames/classNames";
 import cls from "../ui/ThemeSwitcher.module.scss";
 import LightIcon from "/src/shared/assets/icons/theme-light.svg";
 import DarkIcon from "/src/shared/assets/icons/theme-dark.svg";
+import { Button, ThemeButton } from "src/shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -12,12 +13,13 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
   const {theme, toggleTheme} = useTheme();
   
   return (
-    <button
+    <Button
+      theme={ThemeButton.CLEAR}
       onClick={toggleTheme}
       className={classNames(cls.ThemeSwitcher, {}, [className])}
     >
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
-    </button>
+    </Button>
   )
 }
 
