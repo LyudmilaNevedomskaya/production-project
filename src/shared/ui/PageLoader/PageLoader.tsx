1f953cfe64517ca8e586dda415fb929a3daf817d
Lyudmila Nevedomskaya <lyudmila@admachtec.com>
2024-05-02 15:59:20 -0700

    Page Loader Component

---
+++ src/shared/ui/PageLoader/PageLoader.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Loader } from '../Loader/Loader';
+import cls from './PageLoader.module.scss';
+
+interface PageLoaderProps {
+  className?: string;
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => (
+    <div className={classNames(cls.PageLoader, {}, [className])}>
+        <Loader />
+    </div>
+);
